65c792ca2e43e7b7fa8e909246d457c21bd5b89d
Priyesh Pandey <priyesh.pandey321@gmail.com>
2022-07-07 07:14:17 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      Hello World
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Contact />}></Route>
